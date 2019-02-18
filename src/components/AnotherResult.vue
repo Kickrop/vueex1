162dc10b944b4d271f93e403abcf6eb8f78e8f46
den <kickrop@gmail.com>
2019-02-18 07:47:00 +0300

    mapgetter (doesn't work)

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,13 +1,27 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+    <dIv>
+        <p>Counter is: {{ doubleCounter }}</p>
+        <p>Number of clicks: {{ stringCounter }}</p>
+    </dIv>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
     export default {
-        computed: {
-            counter() {
-                return this.$store.getters.doubleCounter;
-            }
-        }
+        // computed: {
+        //     counter() {
+        //         return this.$store.getters.doubleCounter;
+        //     },
+        //     clicks() {
+        //         return this.$store.getters.stringCounter;
+        //     }
+        // }
+        computed: 
+            ///{}...mapGetters([
+         mapGetters([
+            'doubleCounter',
+            'stringCounter'
+        ])
+        
     }
 </script>
